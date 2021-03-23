082432fdc23488dfdca7b34f81a7f647d14d9e72
hyemin23 <hm_stom@naver.com>
2021-03-23 15:32:06 +0900

    깃구성

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
-const port =5000;
+const port = 5000;
 
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://hyemin:a4848684@reactnodeproject.cg6ah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-    useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
-}).then(()=>{
-    console.log("MogoDB Connected..");
-}).catch((err)=> console.log(err));
-
-app.get('/',(req,res) => res.send('Hello World ~ '));
-app.listen(port, () =>console.log(`example app liste on port ${port}!`))
+app.get("/", (req, res) => res.send("hello wordl"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
